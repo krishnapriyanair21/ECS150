6b90da8d2500539209c51a98babe79f02b0432d0
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-13 21:44:26 -0700

    MADE IT INTO A FUNCTION!!!!

--- project2/processes-shell/wish.cpp
+++ project2/processes-shell/wish.cpp
@@ -22,82 +22,31 @@ vector<string> parse(string rawInput){ // from stack overflow
 
 void printError0(){
     cerr<<"An error has occurred"<<endl;
-    cout <<"printed"<<endl;
     exit(0);
 }
+void modes(vector<string> parsedInput);
 int main (int argc, char *argv[]){
 
     vector<string> parsedInput;
     string rawInput;
 
     if (argc == 1){ // interactive
+        cout << "wish> ";
         while (getline(cin, rawInput)){ 
             parsedInput = parse(rawInput);
-            for (unsigned int i = 0; i < parsedInput.size(); i++){ 
-                if (parsedInput[i] == "exit"){  
-                    exit(0);
-                }
-                else if (parsedInput[i] == "cd"){
-                    if (parsedInput.size() <= (i + 1)){
-                        printError0();
-                    }else{
-                        string convertString = parsedInput[i + 1];
-                        int destSize = convertString.size();
-                        char dirToChange[destSize + 1];
-                        strcpy(dirToChange, convertString.c_str()); // need char array for chdir()
-                        if (chdir(dirToChange) == 0){
-                            continue;
-                        } else{
-                            printError0();
-                        }     
-                    }
-                    break;
-                }
-                else if (parsedInput[i] == "ls"){
-                    // check if next input starts with
-                    char *args[0];
-                    args[0] = strdup("ls");
-                    execvp(args[0], args);
-                    return 1;
-                }
-            }
+            modes(parsedInput);
             cout<<"wish> ";
         }
     }else if (argc > 1){ // batch
         for (int i = 1; i < argc; i++){
             ifstream inputFile;
             inputFile.open(argv[i]);
-            
-            parsedInput = parse(rawInput);
-            for (unsigned int i = 0; i < parsedInput.size(); i++){ 
-                if (parsedInput[i] == "exit"){  
-                    exit(0);
-                }
-                else if (parsedInput[i] == "cd"){
-                    if (parsedInput.size() <= (i + 1)){
-                        printError0();
-                    }else{
-                        string convertString = parsedInput[i + 1];
-                        int destSize = convertString.size();
-                        char dirToChange[destSize + 1];
-                        strcpy(dirToChange, convertString.c_str()); // need char array for chdir()
-                        if (chdir(dirToChange) == 0){
-                            continue;
-                        } else{
-                            printError0();
-                        }     
-                    }
-                    break;     
-                }
-                else if (parsedInput[i] == "ls"){
-                    // check if next input starts with
-                    char *args[0];
-                    args[0] = strdup("ls");
-                    execvp(args[0], args);
-                    return 1;
-                }
+                        
+            while (getline(inputFile, rawInput)){
+                parsedInput = parse(rawInput);
+                modes(parsedInput);
+                inputFile.close();
             }
-            inputFile.close();
         }
     }
     
@@ -105,3 +54,33 @@ int main (int argc, char *argv[]){
     return 0;
 }
 
+void modes(vector<string> parsedInput){
+    for (unsigned int i = 0; i < parsedInput.size(); i++){ 
+        if (parsedInput[i] == "exit"){  
+            exit(0);
+        }
+        else if (parsedInput[i] == "cd"){
+            if (parsedInput.size() <= (i + 1)){
+                printError0();
+            }else{
+                string convertString = parsedInput[i + 1];
+                int destSize = convertString.size();
+                char dirToChange[destSize + 1];
+                strcpy(dirToChange, convertString.c_str()); // need char array for chdir()
+                if (chdir(dirToChange) == 0){
+                    continue;
+                } else{
+                    printError0();
+                }     
+            }
+            break;
+        }
+        else if (parsedInput[i] == "ls"){
+            // check if next input starts with
+            char *args[0];
+            args[0] = strdup("ls");
+            execvp(args[0], args);
+        }
+    }
+    return;
+}
