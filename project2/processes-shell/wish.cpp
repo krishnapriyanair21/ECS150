96438f1786c1d0e242ef05ba40b33fcb8e6c577a
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-13 19:54:26 -0700

    parse new interactive and batch

--- project2/processes-shell/wish.cpp
+++ project2/processes-shell/wish.cpp
@@ -1,8 +1,3 @@
-/// if (wish)
-/// while (getline != bad)
-
-/// do stuff
-
 #include <cstring>
 #include <iostream>
 #include <string>
@@ -11,66 +6,62 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <vector>
+#include <sstream>
+#include <fstream>
 using namespace std;
 
+vector<string> parse(string rawInput){ // from stack overflow
+    istringstream s(rawInput);
+    vector<string> parsedInput;
+    string cmdx; // command at x 
+    while(s >> cmdx){
+        parsedInput.push_back(cmdx);
+    }
+    return parsedInput;
+}
 
-
-/// taken from https://www.fluentcpp.com/2017/04/21/how-to-split-a-string-in-c/
-// std::vector<std::string> parseInput(const std::string& rawinput, char delimiter)
-// {
-//    std::vector<std::string> parsedInput;
-//    std::string rawInput;
-//    std::istringstream inputStream(s);
-//    while (std::getline(inputStream, rawInput, delimiter))
-//    {
-//       parsedInput.push_back(token);
-//    }
-//    return parsedInput;
-// }
 int main (int argc, char *argv[]){
 
     cout<<"wish> ";
 
     vector<std::string> parsedInput;
-    std::string rawInput;
-    std::string inputForPushBack; 
+    string rawInput;
 
-    while (getline(cin, rawInput)){
-        unsigned int startChar = 0; // change startChar to zero every new input
-        for (unsigned int i = 0; i < rawInput.length(); i++){
-            if (rawInput[i] == ' ' ){
-                inputForPushBack = rawInput.substr(startChar, i);
-                startChar = (i + 1); 
-                cout << startChar << " is start char" <<endl;
-                parsedInput.push_back(inputForPushBack);
-            }
-            else if (i == (rawInput.length() - 1)) { // if nothing else PUSH IT
-                cout << startChar << " is start char in else if" <<endl;
-                inputForPushBack = rawInput.substr(startChar, i + 1); // startChar == 0????  WHY 
-                parsedInput.push_back(inputForPushBack);                
-            }
-            else{
-                continue; /// add case where multiple spaces or tabs or newlines all in first if 
-            }
-            cout << startChar<<" startChar outside for loop" <<endl;
-        }
-        cout<<inputForPushBack<< " is input for push back" <<endl; 
-
-        cout << parsedInput.size() << " is size" <<endl;
-        // need to track last iteration input point and run from that point to size
-            for (unsigned int i = 0; i < parsedInput.size(); i++){
-                if (parsedInput[i] == "exit"){  /// iterate through every previous command 
-                    return 0;
+    if (argc == 1){ // interactive
+        while (getline(cin, rawInput)){ /// loop until exit
+            cout <<"rawInput: " << rawInput<<endl;
+            parsedInput = parse(rawInput);
+            for (unsigned int i = 0; i < parsedInput.size(); i++){ // iterate through every previous command 
+                cout << "testing! " << parsedInput[i] <<" at " << i <<endl;
+                if (parsedInput[i] == "exit"){  
+                    exit(0);
                 }
                 else if (parsedInput[i] == "cd"){
-                    cout<< "cd";
-                }
-                else{
-                    cout << parsedInput[i] << " is parsedInput[i]" <<endl;
-                    
+                    // cout<< "cd";
+                    continue;
                 }
             }
             cout<<"wish> ";
+        }
+    }else{ // batch
+        for (int i = 1; i < argc; i++){
+            ifstream inputFile;
+            inputFile.open(argv[i]);
+            while(getline(inputFile, rawInput)){
+                parsedInput = parse(rawInput);
+                for (unsigned int i = 0; i < parsedInput.size(); i++){ // iterate through every previous command 
+                if (parsedInput[i] == "exit"){  
+                    exit(0);
+                }
+                else if (parsedInput[i] == "cd"){
+                    // cout<< "cd";
+                    continue;
+                }
+            }
+            cout<<"wish> ";
+            }
+            inputFile.close();
+        }
     }
     
 
