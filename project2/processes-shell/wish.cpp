888d6f0ba39a632f83eaa48288ce4377d2847944
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-15 12:31:08 -0700

    passed: 1,2,6, 8, 9, 10, 12, 21

--- project2/processes-shell/wish.cpp
+++ project2/processes-shell/wish.cpp
@@ -13,7 +13,7 @@ using namespace std;
 vector<string> parse(string rawInput);
 void printError0();
 void printError1();
-void modes(vector<string> parsedInput);
+void executeCmd(vector<string> parsedInput);
 
 int main (int argc, char *argv[]){
 
@@ -24,7 +24,7 @@ int main (int argc, char *argv[]){
         cout << "wish> ";
         while (getline(cin, rawInput)){ 
             parsedInput = parse(rawInput);
-            modes(parsedInput);
+            executeCmd(parsedInput);
             cout<<"wish> ";
         }
     }else if (argc > 1){ // batch
@@ -34,17 +34,17 @@ int main (int argc, char *argv[]){
                         
             while (getline(inputFile, rawInput)){
                 parsedInput = parse(rawInput);
-                modes(parsedInput);
+                executeCmd(parsedInput);
                 inputFile.close();
             }
         }
-    }
+    } 
     
 
     return 0;
 }
 
-void modes(vector<string> parsedInput){
+void executeCmd(vector<string> parsedInput){
     for (unsigned int i = 0; i < parsedInput.size(); i++){ 
         if (parsedInput[i] == "exit"){  
             exit(0);
@@ -71,6 +71,9 @@ void modes(vector<string> parsedInput){
                 args[1] = strdup("-l");
                 args[2]= NULL;
                 execv(args[0], args); /// does not work 
+        }
+        else{
+            printError0();
         }
     }
     return;
