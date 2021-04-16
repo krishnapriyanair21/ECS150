27f575be50bbaf7b708033dfa7ab6ce23e6c3ede
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-15 18:12:52 -0700

    path function

--- project2/processes-shell/wish.cpp
+++ project2/processes-shell/wish.cpp
@@ -1,3 +1,5 @@
+/* This project was done with Dave Khalasi and Divya Singh */
+
 #include <cstring>
 #include <iostream>
 #include <string>
@@ -13,18 +15,21 @@ using namespace std;
 vector<string> parse(string rawInput);
 void printError0();
 void printError1();
-void executeCmd(vector<string> parsedInput);
-
+void executeCmd(vector<string> parsedInput, vector<string> &path);
+void pathFunction(vector<string> parsedInput, vector<string> &path);
 int main (int argc, char *argv[]){
 
     vector<string> parsedInput;
     string rawInput;
+    vector<string> curPath;
+    curPath.push_back("");
+    curPath.push_back("/bin/");
 
     if (argc == 1){ // interactive
         cout << "wish> ";
         while (getline(cin, rawInput)){ 
             parsedInput = parse(rawInput);
-            executeCmd(parsedInput);
+            executeCmd(parsedInput, curPath);
             cout<<"wish> ";
         }
     }else if (argc > 1){ // batch
@@ -34,7 +39,7 @@ int main (int argc, char *argv[]){
                         
             while (getline(inputFile, rawInput)){
                 parsedInput = parse(rawInput);
-                executeCmd(parsedInput);
+                executeCmd(parsedInput,curPath);
                 inputFile.close();
             }
         }
@@ -44,7 +49,7 @@ int main (int argc, char *argv[]){
     return 0;
 }
 
-void executeCmd(vector<string> parsedInput){
+void executeCmd(vector<string> parsedInput, vector<string> &path){
     for (unsigned int i = 0; i < parsedInput.size(); i++){ 
         if (parsedInput[i] == "exit"){  
             exit(0);
@@ -58,26 +63,39 @@ void executeCmd(vector<string> parsedInput){
                 char dirToChange[destSize + 1];
                 strcpy(dirToChange, convertString.c_str()); // need char array for chdir()
                 if (chdir(dirToChange) == 0){
-                    continue;
+                    vector<string> newDir;
+                    newDir.push_back(convertString);
+                    pathFunction(newDir,path);
                 } else{
                     printError0();
                 }     
             }
             break;
-        } else if (parsedInput[i] == "ls"){
-            // check if next input starts with -
-                char *args[3];
-                args[0] = strdup("ls");
-                args[1] = strdup("-l");
-                args[2]= NULL;
-                execv(args[0], args); /// does not work 
-        }
-        else{
+        } else if (parsedInput[i] == "path") {
+           pathFunction(parsedInput, path);
+        }else{
+            
             printError0();
         }
     }
     return;
 }
+
+void pathFunction(vector<string> parsedInput, vector<string> &path){
+    path.clear();
+    path.push_back("");
+    for (unsigned int i = 1; i < parsedInput.size(); i++){
+        if (parsedInput[i][0] != '/') 
+            parsedInput[i] = "/" + parsedInput [i];
+        if (parsedInput[i][parsedInput.size() - 1] != '/')
+            parsedInput[i] += "/";
+        char curDir[4096];
+        getcwd(curDir, 4096);
+        parsedInput[i] = curDir + parsedInput[i];
+        path.push_back(parsedInput[i]);
+    }
+}
+
 vector<string> parse(string rawInput){  // stack overflow
     istringstream s(rawInput);
     vector<string> parsedInput;
