854100c0f2353c051963f6adc45acf7fff06919f
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-17 17:11:43 -0700

    normal mode

--- project2/processes-shell/wish.cpp
+++ project2/processes-shell/wish.cpp
@@ -9,14 +9,16 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+#include <sys/wait.h>
 
 using namespace std;
 
 vector<string> parse(string rawInput);
-void printError0();
-void printError1();
+void printError();
 void executeCmd(vector<string> parsedInput, vector<string> &path);
 void pathFunction(vector<string> parsedInput, vector<string> &path);
+void executeBuiltIn (vector<string> parsedInput, vector<string> &path);
+
 int main (int argc, char *argv[]){
 
     vector<string> parsedInput;
@@ -33,78 +35,67 @@ int main (int argc, char *argv[]){
             cout<<"wish> ";
         }
     }else if (argc > 1){ // batch
+        if (argc > 3){  // more than 1 file
+            printError();
+        }
         for (int i = 1; i < argc; i++){
             ifstream inputFile;
             inputFile.open(argv[i]);
-                        
-            while (getline(inputFile, rawInput)){
-                parsedInput = parse(rawInput);
-                executeCmd(parsedInput,curPath);
-                inputFile.close();
+            if (!inputFile){
+                printError();
+                exit(1);
+            }  
+            else{
+                while (getline(inputFile, rawInput)){
+                    parsedInput = parse(rawInput);
+                    if (parsedInput.size() > 0){
+                        executeCmd(parsedInput,curPath);
+                    }
+                }
             }
+            inputFile.close();
         }
-    } 
-    
-
+    }
     return 0;
 }
 
 void executeCmd(vector<string> parsedInput, vector<string> &path){
-    // bool redirect = false;
-    // bool para = false;
-    // for (unsigned int i = 0; i < parsedInput.size(); i++){
-    //     if (parsedInput[i] == "&")
-    //         para = true;
-    //     if (parsedInput[i] == ">")
-    //         redirect = true;
-    // }
-    for (unsigned int i = 0; i < parsedInput.size(); i++){ 
-        if (parsedInput[i] == "exit"){  
-            exit(0);
-        } else if (parsedInput[i] == "cd"){
-            if (parsedInput.size() <= (i + 1)){
-                printError0();
-                break;
+        if (parsedInput[0] == "exit"){  
+            if (parsedInput.size() > 1){
+                printError(); 
             }else{
-                string convertString = parsedInput[i + 1];
+                exit(0);
+            }
+        } else if (parsedInput[0] == "cd"){
+            if (parsedInput.size() < 2 ){
+                printError(); 
+            }else{
+                string convertString = parsedInput[1];
                 int destSize = convertString.size();
                 char dirToChange[destSize + 1];
                 strcpy(dirToChange, convertString.c_str()); // need char array for chdir()
-                if (chdir(dirToChange) == 0){
-                    vector<string> newDir;
-                    newDir.push_back(convertString);
-                    pathFunction(newDir,path);
-                } else{
-                    printError0();
+                if (!(chdir(dirToChange) == 0)){
+                    printError();
                 }     
             }
-            break;
-        } else if (parsedInput[i] == "path") {
+        } else if (parsedInput[0] == "path") {
            pathFunction(parsedInput, path);
         }else{
-            char *arg[parsedInput.size() + 1];
-            for (unsigned int j = 0; j < parsedInput.size(); j++){
-                arg[j] = new char[parsedInput[j].size() + 1];
-                strcpy(arg[j], parsedInput[j].c_str());
+            pid_t ret = fork();
+            if (ret == 0){
+                // redirection parallel or normal 
+                executeBuiltIn(parsedInput, path);
+            } else{
+                wait(NULL);
             }
-            arg[parsedInput.size()] = nullptr;
-            for (unsigned int j = 0; j < path.size(); j++){
-                char newCmd[path[j].size() + parsedInput.size() + 1];
-                strcpy(newCmd, (path[j] + parsedInput[0]).c_str());
-                execv(newCmd, arg);
-            }
-            printError0();
         }
-    }
     return;
 }
 
 void pathFunction(vector<string> parsedInput, vector<string> &path){
     path.clear();
     path.push_back("");
-    if (parsedInput.size() == 1){
-        path.push_back("/bin/");
-    }
+
     for (unsigned int i = 1; i < parsedInput.size(); i++){
         if (parsedInput[i][0] != '/') 
             parsedInput[i] = "/" + parsedInput [i];
@@ -126,11 +117,24 @@ vector<string> parse(string rawInput){  // stack overflow
     }
     return parsedInput;
 }
-void printError0(){
+
+void printError(){
     cerr<<"An error has occurred"<<endl;
-    exit(0);
 }
-void printError1(){
-    cerr<<"An error has occurred"<<endl;
-    exit(1);
+
+void executeBuiltIn (vector<string> parsedInput, vector<string> &path){
+    char *arg[parsedInput.size() + 1];
+    for (unsigned int j = 0; j < parsedInput.size(); j++){
+        arg[j] = new char[parsedInput[j].size() + 1];
+        strcpy(arg[j], parsedInput[j].c_str());
+    }
+    arg[parsedInput.size()] = nullptr;
+
+    for (unsigned int j = 0; j < path.size(); j++){
+        char newCmd[path[j].size() + parsedInput.size() + 1];
+        strcpy(newCmd, (path[j] + parsedInput[0]).c_str());
+        execv(newCmd, arg);
+    }
+    printError();
+    exit(1); // cannot execute built in 
 }
